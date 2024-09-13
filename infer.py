29ba0e9492dbf216e035f2d4938245b155e9ef32
WhatAMistake <162622425+WhatAMistake@users.noreply.github.com>
2024-09-13 22:27:07 +0300

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,27 @@
+from keras.models import load_model
+from keras.models import save
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import CategoricalCrossentropy
+from tensorflow.keras.metrics import AUC
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+import tensorflow.keras.utils
+
+earlystopper = EarlyStopping(monitor='val_loss', patience=5)
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=42)
+
+model = Sequential()
+model.add(Dense(64, activation='relu')))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(4, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc', AUC()])
+
+model.fit(X, Y, epochs=50, batch_size=10, validation_split=0.2, callbacks=[earlystopper])
+
+accuracy = model.evaluate(X_test, Y_test)
+print("Accuracy:", accuracy * 100, "%")
